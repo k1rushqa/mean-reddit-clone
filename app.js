a4b7ca384ded28a6539129a628080c8f3b829076
Luke <ljatkinson88@gmail.com>
2015-06-01 19:32:15 -0500

    Wrote basic templates
    Wrote template for index and app.js

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ app.controller('MainCtrl', [
   function($scope){
     $scope.test = 'Hello, world!';
   }]);
+
+$scope.posts = [
+  {title: 'post1', upvotes: 9 },
+  {title: 'post2', upvotes: 2 },
+  {title: 'post3', upvotes: 12 },
+  {title: 'post4', upvotes: 5 },
+  {title: 'post5', upvotes: 4 },
+];
+
