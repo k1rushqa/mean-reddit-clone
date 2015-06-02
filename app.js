b192d116dea056eb3c98cf2df0c6de63cbb33fd8
Luke <ljatkinson88@gmail.com>
2015-06-02 14:31:49 -0500

    Added scope and html structure
    Wrote the posts function, added early upvote sorting and forms.

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-var app =  angular.module('flapperNew', []);
+var app = angular.module('flapperNews', []);
 
 app.controller('MainCtrl', [
   '$scope',
   function($scope){
     $scope.test = 'Hello, world!';
-  }]);
+  
 
 $scope.posts = [
   {title: 'post1', upvotes: 9 },
   {title: 'post2', upvotes: 2 },
   {title: 'post3', upvotes: 12 },
   {title: 'post4', upvotes: 5 },
-  {title: 'post5', upvotes: 4 },
+  {title: 'post5', upvotes: 4 }
 ];
 
+$scope.addPost = function(){
+  if(!$scope.title || $scope.title === '') { return; }
+  $scope.posts.push({title: $scope.title, upvotes: 0});
+                    $scope.title = '';
+};
+
+
+}]);
