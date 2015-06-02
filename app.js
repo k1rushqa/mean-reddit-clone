3b6a0a742823043e3b470ef3403e88243d038e67
Luke <ljatkinson88@gmail.com>
2015-06-02 15:03:13 -0500

    Added styles
    Used BootstrapCDN and Bootswatch for a nice look and layout.

--- app.js
+++ app.js
@@ -4,21 +4,28 @@ app.controller('MainCtrl', [
   '$scope',
   function($scope){
     $scope.test = 'Hello, world!';
-  
 
-$scope.posts = [
-  {title: 'post1', upvotes: 9 },
-  {title: 'post2', upvotes: 2 },
-  {title: 'post3', upvotes: 12 },
-  {title: 'post4', upvotes: 5 },
-  {title: 'post5', upvotes: 4 }
-];
+    $scope.posts = [
+      {title: 'post1', upvotes: 9 },
+      {title: 'post2', upvotes: 2 },
+      {title: 'post3', upvotes: 12 },
+      {title: 'post4', upvotes: 5 },
+      {title: 'post5', upvotes: 4 }
+    ];
 
-$scope.addPost = function(){
-  if(!$scope.title || $scope.title === '') { return; }
-  $scope.posts.push({title: $scope.title, upvotes: 0});
-                    $scope.title = '';
-};
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
+      $scope.title = '';
+      $scope.link = '';
+    };
 
+    $scope.incrementUpvotes = function(post){
+      post.upvotes += 1;
+    };
 
-}]);
+  }]);
