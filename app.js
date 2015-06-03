64f0f5a0086e3552b340d700a7aa259115670e32
Luke <ljatkinson88@gmail.com>
2015-06-03 14:48:34 -0500

    Added Posts page template
    Added the template for the posts page and added a link for comments.
    Created states for the posts page (app.js).

--- app.js
+++ app.js
@@ -7,32 +7,40 @@ app.factory('posts', [function(){
     posts: []
   };
   return o;
-  
+
 }]);
 
 //configure ui-router
 app.config([
-'$stateProvider',
-'$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
-
-  $stateProvider
-    .state('home', {
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    
+    //home state
+    $stateProvider
+      .state('home', {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
     });
+    
+    //posts state
+    $stateProvider
+      .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });  
+    $urlRouterProvider.otherwise('home');
+  }]);
 
-  $urlRouterProvider.otherwise('home');
-}]);
-
-// defining $scope
+// defining main $scope
 app.controller('MainCtrl', [
   '$scope',
   'posts',
   function($scope, posts){
     $scope.test = 'Hello, world!';
-    
+
     //placeholder posts
     $scope.posts = posts.posts;
 
@@ -42,7 +50,11 @@ app.controller('MainCtrl', [
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0
+        upvotes: 0,
+        comments: [
+        {author: 'Joe', body: 'Great post!', upvotes: 2},
+        {author: 'Bill', body: 'Bad post!', upvotes: 0},
+        ]
       });
       $scope.title = '';
       $scope.link = '';
@@ -54,3 +66,12 @@ app.controller('MainCtrl', [
     };
 
   }]);
+
+//defining posts $scope
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
+}]);
